909dd7ff5c9676d9cf569714bc129fbf4eadb102
Leo Barnett <leo.e.barnett@icloud.com>
2023-12-07 13:04:21 +0200

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Leo  on 07.12.23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
